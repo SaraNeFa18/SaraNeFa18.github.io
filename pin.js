b7c1314e6491db540c93a1ef7886954876c630a3
Fleep14 <76646014+Fleep14@users.noreply.github.com>
2024-07-29 19:20:29 +0200

    Update pin.js

--- pin.js
+++ pin.js
@@ -2,10 +2,16 @@
 // Correct Pin Value
 let correctPin = "1234";
 
-let mappa= new Map();
-mappa.set("1234","miao miao")
-mappa.set("0000","bau bau")
-mappa.set("9999",'<iframe width="560" height="315" src="https://www.youtube.com/embed/B5CZw7Z13y4?si=m1s9wzUff6dRIkMC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+let mappa = new Map();
+
+function riempiMappa(){
+    mappa.set("1234","miao miao")
+    mappa.set("0000","bau bau")
+    mappa.set("9999",'<iframe width="560" height="315" src="https://www.youtube.com/embed/B5CZw7Z13y4?si=m1s9wzUff6dRIkMC" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>')
+    
+}
+
+riempiMappa()
 
 let btns =
     document.getElementsByClassName(
